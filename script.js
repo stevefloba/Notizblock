8d1160984b1791b064118ca1c6f5cc75f22e5c4b
Stefan Flosbach <stefanflosbach@t-online.de>
2025-08-30 13:15:59 +0200

    Notizen etwas schicker anzeigen lassen

--- script.js
+++ script.js
@@ -4,8 +4,25 @@ let notes = ['banana', 'rasen mähen'];
 
 function renderNotes() {
     let contentRef = document.getElementById('content');
-    contentRef.innerHTML = notes
+    contentRef.innerHTML = "";
+
+    for (let indexNote = 0; indexNote < notes.length; indexNote++) {
+        const note = notes[indexNote];
+        contentRef.innerHTML += getNoteTemplate(note);
+    }
 }
+
+function getNoteTemplate(note){
+    return `<p>+ ${note}</p>`;
+}
+
+
+
+
+
+
+
+
 
 
 // Notizen hinzufügen
