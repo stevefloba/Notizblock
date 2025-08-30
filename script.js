68e7912506a05c6288b21ac5957a17090b28371b
Stefan Flosbach <stefanflosbach@t-online.de>
2025-08-30 12:51:13 +0200

    Notizen anzeigen lassen

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+let notes = ['banana', 'rasen mähen'];
+
+function renderNotes() {
+    let contentRef = document.getElementById('content');
+    contentRef.innerHTML = notes
+}
+
+
+// Notizen hinzufügen
+// Notizen löschen
+// Notizen archivieren
